587f638897f73d8e535adbccbf5ecccebfd72055
Dr0pG <divenancio@hotmail.com>
2025-04-21 18:22:05 +0100

    Metrics and Shadow

---
+++ constants/Metrics.ts
@@ -0,0 +1,35 @@
+import { Dimensions } from "react-native";
+
+export const Metrics = {
+  // Margins
+  smallMargin: 8,
+  mediumMargin: 16,
+  largeMargin: 24,
+
+  // Font Sizes
+  size10: 10,
+  size12: 12,
+  size14: 14,
+  size16: 16,
+  size18: 18,
+  size20: 20,
+  size22: 22,
+  size24: 24,
+  size26: 26,
+  size28: 28,
+  size30: 30,
+  size32: 32,
+  size34: 34,
+  size36: 36,
+  size38: 38,
+  size40: 40,
+
+  // Border Radius
+  smallRadius: 4,
+  mediumRadius: 8,
+  largeRadius: 16,
+
+  // Screen Dimensions
+  screenWidth: Dimensions.get("window").width,
+  screenHeight: Dimensions.get("window").height,
+};
